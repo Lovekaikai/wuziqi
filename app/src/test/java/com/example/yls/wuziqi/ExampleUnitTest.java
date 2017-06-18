2df2804f3158e8c17253197c2b89f4b90dca8e11
yls <1145275516@qq.com>
2017-06-18 08:46:45 +0800

    kaikai

---
+++ app/src/test/java/com/example/yls/wuziqi/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+package com.example.yls.wuziqi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
